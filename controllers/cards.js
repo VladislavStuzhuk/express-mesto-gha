747f2d46adfa6bb3ff33aa94c518e6866786b654
Vlad Stuzhuk <vladst002@gmail.com>
2022-08-01 22:21:16 +0300

    sprint 13

---
+++ controllers/cards.js
@@ -0,0 +1,44 @@
+const Card = require('../models/card');
+
+module.exports.createCard = (req, res) => {
+  const {
+    name, link,
+  } = req.body;
+  const owner = req.user._id;
+  Card.create({name, link, owner })
+    .then(card =>{ res.send({data: card})
+  })
+    .catch(err => res.status(500).send({ message: ` Произошла ошибка` }));
+};
+
+module.exports.getCards = (req, res) => {
+  Card.find({})
+    .populate('')
+    .then((card) => res.send({ data: card }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+}
+module.exports.deleteCard = (req,res) => {
+  console.log(req.params);
+  Card.findByIdAndRemove(req.params.cardId)
+      .then(card => res.send({ data: card }))
+      .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+}
+module.exports.removeLike = (req,res) => {
+  console.log(req.params);
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } }, // убрать _id из массива
+    { new: true },
+  )
+      .then(card => res.send({ data: card }))
+      .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+}
+module.exports.setLike = (req,res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } }, // убрать _id из массива
+    { new: true },
+  )
+      .then(card => res.send({ data: card }))
+      .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+}
---
+++ routes/cards.js
@@ -0,0 +1,15 @@
+const cardsRouter = require('express').Router();
+const {
+  getCards,
+  createCard,
+  deleteCard,
+  removeLike,
+  setLike
+} = require('../controllers/cards');
+
+cardsRouter.get('/', getCards);
+cardsRouter.post('/', createCard);
+cardsRouter.delete('/:cardId', deleteCard);
+cardsRouter.delete('/:cardId/likes', removeLike);
+cardsRouter.put('/:cardId/likes', setLike);
+module.exports = cardsRouter;
