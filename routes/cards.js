1f8b7701c94b0b95c5f4fd7a408b6035bab68600
Vlad <vladst002@gmail.com>
2022-08-08 13:52:48 +0300

    14 sprint add Joi validation

--- routes/cards.js
+++ routes/cards.js
@@ -18,17 +18,17 @@ cardsRouter.post('/', celebrate({
 }), createCard);
 cardsRouter.delete('/:cardId', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().required(),
+    cardId: Joi.string().required().hex().length(24),
   }),
 }), deleteCard);
 cardsRouter.delete('/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().required(),
+    cardId: Joi.string().required().hex().length(24),
   }),
 }), removeLike);
 cardsRouter.put('/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().required(),
+    cardId: Joi.string().required().hex().length(24),
   }),
 }), setLike);
 module.exports = cardsRouter;
